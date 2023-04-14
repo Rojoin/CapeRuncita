6dedb07ff681148ff4037fef4dfeb7851b84ab60
Rojoin <nachoarrastua@gmail.com>
2023-04-13 22:08:42 -0300

    Add UIController to GameScene

---
+++ Assets/Scripts/UiController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiController : MonoBehaviour
+{
+    [SerializeField] private GameObject InGame;
+    [SerializeField] private GameObject EndGame;
+    [SerializeField] private PlayerController player;
+    [SerializeField] private AudioClip gameplayMusic;
+    [SerializeField] private AudioClip gameOverMusic;
+    [SerializeField] private AudioClip previousClip;
+    [SerializeField] private AudioSource music;
+
+    private void Start()
+    {
+        music = SoundManager.Instance.GetMusicSource();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScreenVisibility(InGame,!player.isDead);
+        ScreenVisibility(EndGame,player.isDead);
+        MusicController();
+    }
+
+    private void MusicController()
+    {
+        music.clip = player.isDead ? gameOverMusic : gameplayMusic;
+        if (previousClip != music.clip)
+        {
+            music.Play();
+        }
+
+        previousClip = music.clip;
+    }
+
+    private void ScreenVisibility(GameObject screen, bool status)
+    {
+        screen.SetActive(status);
+    }
+}
